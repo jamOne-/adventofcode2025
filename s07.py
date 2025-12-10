4a9ae5dce7051f7ce2926ae1f86963b5e73c1098
Dominik Rabij <dominikrabij@google.com>
2025-12-10 16:29:46 +0100

    07b

--- s07.py
+++ s07.py
@@ -1,5 +1,5 @@
+import functools
 from common import parse_grid, read_lines, Point
-from collections import deque
 
 
 def main():
@@ -9,32 +9,38 @@ def main():
 
 def solve_a(lines: list[str]) -> int:
     start, grid = _parse_input(lines)
-    visited: set[Point] = set([start])
-    q: deque[Point] = deque([start])
-    split_count = 0
 
-    while q:
-        x, y = q.popleft()
-        new_ps: list[Point] = []
-
+    @functools.cache
+    def _count_splits(p: Point) -> frozenset[Point]:
+        x, y = p
         if (x, y + 1) not in grid:
-            continue
+            return frozenset()
         elif grid[(x, y + 1)] == "^":
-            new_ps = [(x - 1, y + 1), (x + 1, y + 1)]
-            split_count += 1
+            return (
+                frozenset([(x, y + 1)])
+                | _count_splits((x - 1, y + 1))
+                | _count_splits((x + 1, y + 1))
+            )
         else:
-            new_ps = [(x, y + 1)]
+            return _count_splits((x, y + 1))
 
-        for new_p in new_ps:
-            if new_p not in visited and new_p in grid:
-                visited.add(new_p)
-                q.append(new_p)
-
-    return split_count
+    return len(_count_splits(start))
 
 
 def solve_b(lines: list[str]) -> int:
-    return 0
+    start, grid = _parse_input(lines)
+
+    @functools.cache
+    def _count_timelines(p: Point) -> int:
+        x, y = p
+        if (x, y + 1) not in grid:
+            return 1
+        elif grid[(x, y + 1)] == "^":
+            return _count_timelines((x - 1, y + 1)) + _count_timelines((x + 1, y + 1))
+        else:
+            return _count_timelines((x, y + 1))
+
+    return _count_timelines(start)
 
 
 def _parse_input(
