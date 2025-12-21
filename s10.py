c3f734fbec4b8eb3f8f5eaf3df7a6a216c0245c8
Dominik Rabij <dominikrabij@google.com>
2025-12-21 20:42:04 +0100

    10b

--- s10.py
+++ s10.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
+import functools
+from itertools import chain, combinations
+from typing import Iterable, TypeVar
+from collections import defaultdict
 from common import read_lines
-from collections import deque
 
 
 def main():
@@ -10,37 +13,74 @@ def main():
 
 def solve_a(lines: list[str]) -> int:
     machines = _parse_input(lines)
-    return sum(map(_solve_machine, machines))
+    return sum(map(_solve_machine_a, machines))
 
 
 def solve_b(lines: list[str]) -> int:
-    return 0
+    machines = _parse_input(lines)
+    return sum(map(_solve_machine_b, machines))
 
 
-@dataclass
+@dataclass(frozen=True)
+class _Button:
+    lights: tuple[int, ...]
+    pattern: int
+
+
+@dataclass(frozen=True)
 class _Machine:
     goal: int
-    buttons: list[int]
-    joltage: list[int]
+    buttons: list[_Button]
+    joltage: tuple[int, ...]
 
 
-def _solve_machine(machine: _Machine) -> int:
-    q: deque[tuple[int, int]] = deque([(0, 0)])
-    visited: set[int] = set([0])
+def _get_patterns_dict(buttons: list[_Button]) -> dict[int, list[set[_Button]]]:
+    patterns: dict[int, list[set[_Button]]] = defaultdict(list)
+    for bs in _powerset(buttons):
+        pattern = 0
+        for b in bs:
+            pattern ^= b.pattern
+        patterns[pattern].append(set(bs))
+    return patterns
 
-    while q:
-        state, steps = q.popleft()
 
-        if state == machine.goal:
-            return steps
+def _solve_machine_a(machine: _Machine) -> int:
+    patterns_dict = _get_patterns_dict(machine.buttons)
+    return min(map(len, patterns_dict[machine.goal]))
 
-        for button in machine.buttons:
-            new_state = state ^ button
-            if new_state not in visited:
-                visited.add(new_state)
-                q.append((new_state, steps + 1))
 
-    return -1
+def _solve_machine_b(machine: _Machine) -> int:
+    patterns_dict = _get_patterns_dict(machine.buttons)
+
+    @functools.cache
+    def _aux(joltage: tuple[int, ...]) -> int | None:
+        if all(j == 0 for j in joltage):
+            return 0
+        if any(j < 0 for j in joltage):
+            return None
+
+        parity_goal = 0
+        for j in joltage:
+            parity_goal <<= 1
+            parity_goal += j & 1
+
+        candidates: list[int] = []
+
+        for parity_solution in patterns_dict.get(parity_goal, []):
+            smaller_joltage = list(joltage)
+            for b in parity_solution:
+                for l in b.lights:
+                    smaller_joltage[l] -= 1
+            smaller_joltage = tuple(j >> 1 for j in smaller_joltage)
+            smaller_cost = _aux(smaller_joltage)
+            if smaller_cost is not None:
+                candidates.append(smaller_cost * 2 + len(parity_solution))
+
+        return min(candidates, default=None)
+
+    result = _aux(machine.joltage)
+    assert result is not None
+    return result
 
 
 def _parse_input(lines: list[str]) -> list[_Machine]:
@@ -49,12 +89,14 @@ def _parse_input(lines: list[str]) -> list[_Machine]:
         split = line.split(" ")
         goal = _encode_goal(split[0])
         joltage_s = split[-1][1:-1]
-        joltage = list(map(int, joltage_s.split(",")))
-        buttons: list[int] = []
+        joltage = tuple(map(int, joltage_s.split(",")))
+        buttons: list[_Button] = []
         for lights_s in split[1:-1]:
             lights_s = lights_s[1:-1]
-            lights: list[int] = list(map(int, lights_s.split(",")))
-            button = _encode_lights(lights)
+            lights: tuple[int, ...] = tuple(map(int, lights_s.split(",")))
+            button = _Button(
+                lights=lights, pattern=_encode_lights(lights, n=len(joltage))
+            )
             buttons.append(button)
         machines.append(_Machine(goal, buttons, joltage))
 
@@ -63,18 +105,26 @@ def _parse_input(lines: list[str]) -> list[_Machine]:
 
 def _encode_goal(goal: str) -> int:
     result = 0
-    for c in reversed(goal[1:-1]):
+    for c in goal[1:-1]:
         result <<= 1
         if c == "#":
             result += 1
     return result
 
 
-def _encode_lights(lights: list[int]) -> int:
+def _encode_lights(lights: tuple[int, ...], n: int) -> int:
     result = 0
     for i in lights:
-        result |= 1 << i
+        result |= 1 << (n - i - 1)
     return result
+
+
+_T = TypeVar("_T")
+
+
+def _powerset(iterable: Iterable[_T]) -> Iterable[tuple[_T, ...]]:
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
 if __name__ == "__main__":
--- s10_test.py
+++ s10_test.py
@@ -11,4 +11,10 @@ def test_solve_a():
 def test_solve_b():
     actual = solve_b(read_lines("input/10_test.txt"))
 
-    assert actual == 0
+    assert actual == 33
+
+
+def test_solve_b_222():
+    actual = solve_b(["[...] (0,1) (1,2) (0,2) {2,2,2}"])
+
+    assert actual == 3
